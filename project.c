82abff89cbd06f37f904fe78d11f499861f6a02a
shikhagupta9 <42310453+shikhagupta9@users.noreply.github.com>
2018-10-22 15:57:26 +0530

    Update project.c

--- project.c
+++ project.c
@@ -5,16 +5,15 @@
 typedef struct 
 {	char name[50];
 	float price;
-	int hr;
 	int min;
 	//int sec;
-	int key;
+	//int key;
 }item_details;
 
 typedef struct 
-{	struct item_details food_items[food_item_size];
-	struct item_details special_items[special_item_size];
-	struct item_details special_day[7];	
+{	 item_details food_items[food_item_size];
+	 item_details special_items[special_item_size];
+	 item_details special_day[7];	
 	char name[100];
 	char address[100];	//stores only area of the restaurant
 	//int key;	optional
