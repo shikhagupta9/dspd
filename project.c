6fecb5be647e3f5f4cf76f18cb206bc6cfd5a70c
shikhagupta9 <42310453+shikhagupta9@users.noreply.github.com>
2018-10-22 16:52:35 +0530

    Update project.c

--- project.c
+++ project.c
@@ -2,21 +2,21 @@
 #define food_item_size 20
 #define special_item_size 10
 
-struct item_details
+typedef struct 
 {	char name[50];
 	float price;
 	int hr;
 	int min;
 	//int sec;
 	int key;
-};
+}item_details;
 
-struct restaurant
-{	struct item_details food_items[food_item_size];
-	struct item_details special_items[special_item_size];
-	struct item_details special_day[7];	
+typedef struct 
+{	item_details food_items[food_item_size];
+	item_details special_items[special_item_size];
+	item_details special_day[7];	
 	char name[100];
 	char address[100];	//stores only area of the restaurant
 	//int key;	optional
-};
+}restaurant;
 
